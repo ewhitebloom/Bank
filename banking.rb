47bb2cbffed5b1bc6bfb123fb8abfe3bd39a00e0
ewhitebloom <ewhitebloom@gmail.com>
2014-03-04 16:01:13 -0500

    Free o' dependencies?

--- banking.rb
+++ banking.rb
@@ -6,19 +6,20 @@ def format_currency(currency)
   sprintf('%.2f', currency)
 end
 
-def feedback(account_name, transactions, accounts)
+def feedback(account_name, transactions, transaction_data, accounts)
   puts "===== #{account_name} ======\n\n"
   puts "Starting Balance: $" + format_currency(accounts.starting_balance("#{account_name}")).to_s
   puts "Ending Balance: $" +  format_currency(accounts.ending_balance("#{account_name}")).to_s + "\n\n"
-  return_data = BankTransaction.new.summary(account_name.to_s, transactions)
+  return_data = transactions.summary(account_name.to_s, transaction_data)
   return_data.each do |transaction|
     puts "$#{format_currency(transaction[0])} #{transaction[1]} #{transaction[2]} - #{transaction[3]}"
   end
   puts "\n==========================================\n\n"
 end
 
-transactions = BankTransaction.new.balances('balances.csv')
-accounts = BankAccount.new(transactions,'balances.csv')
+transactions = BankTransaction.new
+transaction_data = transactions.balances('balances.csv')
+accounts = BankAccount.new(transaction_data,'balances.csv')
 
-feedback("Purchasing Account", transactions, accounts)
-feedback("Business Checking", transactions, accounts)
+feedback("Purchasing Account", transactions, transaction_data, accounts)
+feedback("Business Checking", transactions, transaction_data, accounts)

b6cac7708d7669191f7e155e033318a9a809fb2e
ewhitebloom <ewhitebloom@gmail.com>
2014-03-04 15:24:26 -0500

    Trimmed

--- banking.rb
+++ banking.rb
@@ -6,10 +6,10 @@ def format_currency(currency)
   sprintf('%.2f', currency)
 end
 
-def feedback(account_name, transactions, account)
-  puts "===== #{account_name} ======"
-  puts "Starting Balance: $" + format_currency(account.starting_balance("#{account_name}")).to_s
-  puts "Ending Balance: $" +  format_currency(account.ending_balance("#{account_name}")).to_s + "\n"
+def feedback(account_name, transactions, accounts)
+  puts "===== #{account_name} ======\n\n"
+  puts "Starting Balance: $" + format_currency(accounts.starting_balance("#{account_name}")).to_s
+  puts "Ending Balance: $" +  format_currency(accounts.ending_balance("#{account_name}")).to_s + "\n\n"
   return_data = BankTransaction.new.summary(account_name.to_s, transactions)
   return_data.each do |transaction|
     puts "$#{format_currency(transaction[0])} #{transaction[1]} #{transaction[2]} - #{transaction[3]}"
@@ -18,7 +18,7 @@ def feedback(account_name, transactions, account)
 end
 
 transactions = BankTransaction.new.balances('balances.csv')
-account = BankAccount.new(transactions,'balances.csv')
+accounts = BankAccount.new(transactions,'balances.csv')
 
-feedback("Purchasing Account", transactions, account )
-feedback("Business Checking", transactions, account)
+feedback("Purchasing Account", transactions, accounts)
+feedback("Business Checking", transactions, accounts)
